f36f336b155d9425f001fef2e02a55d1e129a4cb
unknown <christianmolicki@gmail.com>
2024-02-03 18:21:58 +0100

    add history

--- gemini.py
+++ gemini.py
@@ -2,12 +2,13 @@ import aiohttp, aiohttp_socks, json, base64, os
 from PIL import Image
 from datetime import datetime
 import io
-async def gemini(prompt: str, apikey: str, connector: aiohttp.TCPConnector | aiohttp_socks.ProxyConnector = aiohttp.TCPConnector(), image: str | bytes | io.BufferedReader = None):
+async def gemini(prompt: str, apikey: str, connector: aiohttp.TCPConnector | aiohttp_socks.ProxyConnector = None, image: str | bytes | io.BufferedReader = None, history: list[dict] = None):
     """
     prompt (str): prompt to give [required]
     apikey (str): api key to use [get one here](https://makersuite.google.com/app/apikey) [required]
     connector (aiohttp.TCPConnector | aiohttp_socks.ProxyConnector): connector to use (ignore if you dont know) [optional]
     image (str | bytes | io.BufferedReader): filepath/link/bytes/reader to an image to use with gemini pro vision [optional]
+    history (list[dict]): history to provide to gemini, format: [{"role": "user", "text": "hello world"}, {"role": "model", "text": "greetings!"}]
     """
     
     headers = {
@@ -17,18 +18,36 @@ async def gemini(prompt: str, apikey: str, connector: aiohttp.TCPConnector | aio
     params = {
         'key': apikey,
     }
-
-    json_data = {
-        'contents': [
-            {
-                'parts': [
-                    {
-                        'text': prompt,
-                    },
-                ],
-            },
-        ],
-    }
+    if history and image:
+        raise ValueError("cant do image and history")
+    if not history:
+        json_data = {
+            'contents': [
+                {
+                    'parts': [
+                        {
+                            'text': prompt,
+                        },
+                    ],
+                },
+            ],
+        }
+    else:
+        parsed = []
+        for i in history:
+            if not isinstance(i, dict):
+                raise ValueError("bad history parsing: not dict")
+            role = i.get('role')
+            if not role:
+                raise ValueError("bad history parsing: no role")
+            htext = i.get('text')
+            if not htext:
+                raise ValueError("bad history parsing: no text")
+            parsed.append({"role": role, "parts": [{"text": htext}]})
+        parsed.append({"role": "user", "parts": [{"text": prompt}]})
+        json_data = {
+            'contents': parsed
+        }
     if image:
         islink: bool = False
         if not isinstance(image, bytes) and not isinstance(image, io.BufferedReader) and not os.path.exists(image):
@@ -44,15 +63,15 @@ async def gemini(prompt: str, apikey: str, connector: aiohttp.TCPConnector | aio
                     image = "image"
                     islink = True
             else:
-                yield "cant find image"
+                raise ValueError("cant find image")
         if isinstance(image, io.BufferedReader):
             readbytes = image.read()
         img = Image.open(io.BytesIO(image) if isinstance(image, bytes) else image if isinstance(image, str) else io.BytesIO(readbytes))
         imgformat = img.format
         if not imgformat:
-            yield "invalid image"
+            raise ValueError("invalid image")
         if imgformat.lower() not in ["png", "jpeg", "jpg", "webp", "heic", "heif"]:
-            yield 'not valid image format, needs to be one of these: "png", "jpeg", "jpg", "webp", "heic", "heif"'
+            raise ValueError('not valid image format, needs to be one of these: "png", "jpeg", "jpg", "webp", "heic", "heif"')
         img.close()
         if isinstance(image, str):
             data = base64.b64encode(open(image, 'rb').read()).decode("utf-8")
@@ -70,8 +89,15 @@ async def gemini(prompt: str, apikey: str, connector: aiohttp.TCPConnector | aio
         }}
         json_data["contents"][0]["parts"].append(imgdata)
     temp = ""
+    mainurl = 'https://generativelanguage.googleapis.com/v1/models/gemini-pro:streamGenerateContent'
+    if image and not history:
+        mainurl = 'https://generativelanguage.googleapis.com/v1/models/gemini-pro-vision:streamGenerateContent'
+    elif history:
+        mainurl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:streamGenerateContent"
+    if not connector:
+        connector = aiohttp.TCPConnector()
     async with aiohttp.ClientSession(connector=connector) as session:
-        async with session.post('https://generativelanguage.googleapis.com/v1/models/gemini-pro:streamGenerateContent' if not image else 'https://generativelanguage.googleapis.com/v1/models/gemini-pro-vision:streamGenerateContent', params=params, headers=headers, json=json_data) as response:
+        async with session.post(mainurl, params=params, headers=headers, json=json_data) as response:
             while True:
                 chunk = await response.content.read(1024*10)
                 if not chunk:
@@ -79,6 +105,7 @@ async def gemini(prompt: str, apikey: str, connector: aiohttp.TCPConnector | aio
                 if chunk == b']':
                     continue
                 decoded = chunk.decode("utf-8")
+
                 if decoded.startswith("[") or decoded.startswith(","):
                     decoded = decoded[1:]
                 try:
@@ -97,7 +124,17 @@ async def gemini(prompt: str, apikey: str, connector: aiohttp.TCPConnector | aio
                     temp += decoded
 async def main():
     with open("response.txt", "w") as f1:
-        async for text in gemini("describe this image", apikey, aiohttp_socks.ProxyConnector.from_url(proxy), image=open("image.jpeg", "rb").read()):
+        history = [
+            {
+                "role": "user",
+                "text": "what day is it?"
+            },
+            {
+                "role": "model",
+                "text": "it is a saturday!"
+            },
+        ]
+        async for text in gemini("how do you know this?", apikey, history=history, connector=aiohttp_socks.ProxyConnector.from_url(proxy)):
             print(text)
             f1.write(text)
 if __name__ == "__main__":
