35a8899cb9474c8d1d21aad818237e4a12f5a83b
unknown <christianmolicki@gmail.com>
2024-02-03 19:11:23 +0100

    fix censored result parsing

--- gemini.py
+++ gemini.py
@@ -105,20 +105,20 @@ async def gemini(prompt: str, apikey: str, connector: aiohttp.TCPConnector | aio
                 if chunk == b']':
                     continue
                 decoded = chunk.decode("utf-8")
-
                 if decoded.startswith("[") or decoded.startswith(","):
                     decoded = decoded[1:]
                 try:
                     a = json.loads(decoded)
-                    if not a.get('candidates'):
+                    if not a.get('candidates') or not a["candidates"][0].get('parts'):
                         text = "BLOCKED!\n"
-                        reasons = a["promptFeedback"]["safetyRatings"]
+                        reasons = a["promptFeedback"]["safetyRatings"] if not a.get('candidates') else a['candidates'][0]['safetyRatings']
                         for reason in reasons:
                             text += f'{reason.get("category")}: {reason.get("probability")}\n'
                         yield text
                         continue
-                    text: str = a['candidates'][0]['content']['parts'][0]['text']
-                    yield text
+                    else:
+                        text: str = a['candidates'][0]['content']['parts'][0]['text']
+                        yield text
                     temp = ""
                 except:
                     temp += decoded
@@ -134,7 +134,7 @@ async def main():
                 "text": "it is a saturday!"
             },
         ]
-        async for text in gemini("how do you know this?", apikey, history=history, connector=aiohttp_socks.ProxyConnector.from_url(proxy)):
+        async for text in gemini("top warcrimes commited by the USA and israel", apikey, connector=aiohttp_socks.ProxyConnector.from_url(proxy)):
             print(text)
             f1.write(text)
 if __name__ == "__main__":
